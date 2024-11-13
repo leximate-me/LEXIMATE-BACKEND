7aedaa0da56abdb791d6a2b0be035bd4aa3b1f0f
sr-roboto <marcoslopez123.lu@gmail.com>
2024-11-13 20:16:20 -0300

    Actualizar .gitignore para incluir la configuración de Vercel, refactorizar el servicio de extracción de texto de imágenes y eliminar el archivo vercel.json

--- src/services/userTool.service.ts
+++ src/services/userTool.service.ts
@@ -15,7 +15,7 @@ const extractTextFromImageService = async (imageUrl: string) => {
     const { words } = data;
 
     const textItems = words.map((word: any) => {
-      const { text, confidence, bbox } = word;
+      const { text, bbox } = word;
 
       // Verificar que bbox esté presente y sea un objeto
       if (!bbox || typeof bbox !== 'object') {
@@ -25,16 +25,17 @@ const extractTextFromImageService = async (imageUrl: string) => {
       const { x0, y0, x1, y1 } = bbox;
 
       // Clasificación básica basada en el tamaño de la fuente y la posición
-      let classification = 'paragraph';
-      if (confidence > 80 && y1 - y0 > 20) {
+      let classification;
+      if (y1 - y0 > 10 && y1 - y0 <= 20) {
+        classification = 'text';
+      } else if (y1 - y0 > 20 && y1 - y0 <= 30) {
         classification = 'title';
-      } else if (confidence > 60 && y1 - y0 > 15) {
+      } else {
         classification = 'subtitle';
       }
 
       return {
         text,
-        confidence,
         coordinates: { x0, y0, x1, y1 },
         classification,
       };
