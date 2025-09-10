91c540b74fc301a18909fbf25a8987ce7070df68
sr-roboto <marcoslopez123.lu@gmail.com>
2025-09-10 16:29:15 -0300

    feat: :sparkles: Agregar clases DTO y entidad para usuarios y personas

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  role: string;
+  first_name: string;
+  last_name: string;
+  dni: string;
+  institute: string;
+  user_name: string;
+  email: string;
+  phone_number: string;
+  birth_date: string;
+  password: string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    type: 'text',
+  })
+  user_name: string;
+  @Column({
+    type: 'text',
+  })
+  email: string;
+  @Column({
+    type: 'text',
+  })
+  password: string;
+  @Column({
+    type: 'boolean',
+  })
+  verified: boolean;
+}
