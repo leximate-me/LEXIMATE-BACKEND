745c3a5efbccaf66f6997a56bd6c27c210ee72d9
sr-roboto <marcoslopez123.lu@gmail.com>
2025-10-24 17:48:46 -0300

    refactor: simplificar la clase App y mejorar la estructura del código

--- src/app.ts
+++ src/app.ts
@@ -8,18 +8,19 @@ import { logger } from './common/configs/logger.config';
 import 'reflect-metadata';
 import figlet from 'figlet';
 
-// Creamos una clase y la exportamos
 export class App {
-  private app: Application;
+  private app: Application = express();
 
-  private port: number | string;
+  constructor(private port: string) {
+    this.applyMiddlewares();
+    this.setRoutes();
+    this.settings();
+  }
 
-  // Función para aplicar los middlewares de la aplicación
   private applyMiddlewares() {
     applyMiddlewares(this.app);
   }
 
-  // Función para setear las rutas de la aplicación
   private setRoutes() {
     this.app.use('/api/auth', authRouter);
     this.app.use('/api/class', classRouter);
@@ -27,22 +28,11 @@ export class App {
     this.app.use('/api/post', postRouter);
   }
 
-  // Constructor de la clase App
-  constructor(port: number | string) {
-    this.app = express();
-    this.port = port;
-    this.applyMiddlewares();
-    this.setRoutes();
-    this.settings();
-  }
-
-  // Configuración para setear el puerto del servidor obteniendo el parametro de la funcion
   private settings() {
     this.app.set('port', this.port || process.env.PORT);
   }
 
-  // Función para poner en escucha al servidor
-  listen() {
+  public listen() {
     this.app.listen(this.app.get('port'));
     figlet.text(
       'LEXIMATE',
@@ -53,7 +43,6 @@ export class App {
           return;
         }
 
-        // Imprimimos por consola el logo de 'LEXIMATE' creado como un texto ASCII
         logger.info('\n' + data);
 
         logger.info(`Servidor corriendo en el puerto: ${this.port}`);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,11 @@ import { App } from './app';
 import { connectDB } from './database/db';
 
 async function main() {
-  // Creamos una nueva instancia de la clase App y le pasamos el puerto
   const app = new App(PORT);
 
-  // Conectamos a la base de datos
   await connectDB();
 
-  // Ponemos en escucha al servidor
   app.listen();
 }
 
-main(); // Inicializamos la aplicación
+main();
