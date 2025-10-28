abf2ee88c96b8849ec2fabccbe9b6edebee6e99b
sr-roboto <marcoslopez123.lu@gmail.com>
2025-10-28 07:35:45 -0300

    refactor(post): renombrar repositorios y mejorar la gestión de errores en los métodos de creación, lectura, actualización y eliminación

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -7,83 +7,83 @@ import { CreatePostDto } from './dtos/create-post.dto';
 import { UpdatePostDto } from './dtos/update-post.dto';
 
 export class PostService {
-  private readonly classRepo = AppDataSource.getRepository(Course);
-  private readonly userRepo = AppDataSource.getRepository(User);
-  private readonly postRepo = AppDataSource.getRepository(Post);
+  private readonly courseRepository = AppDataSource.getRepository(Course);
+  private readonly userRepository = AppDataSource.getRepository(User);
+  private readonly postRepository = AppDataSource.getRepository(Post);
 
-  async create(createPostDto: CreatePostDto, classId: string, userId: string) {
-    const existingClass = await this.classRepo.findOne({
-      where: { id: classId },
+  async create(createPostDto: CreatePostDto, courseId: string, userId: string) {
+    const existingCourse = await this.courseRepository.findOne({
+      where: { id: courseId },
       relations: ['users'],
     });
-    if (!existingClass) throw HttpError.notFound('Clase no encontrada');
+    if (!existingCourse) throw HttpError.notFound('Clase no encontrada');
 
-    const foundUser = await this.userRepo.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['courses'],
     });
     if (!foundUser) throw HttpError.notFound('Usuario no encontrado');
 
-    const isInClass = foundUser.courses.some((c) => c.id === classId);
-    if (!isInClass)
+    const isInCourse = foundUser.courses.some((c) => c.id === courseId);
+    if (!isInCourse)
       throw HttpError.forbidden('El usuario no pertenece a la clase');
 
-    const post = this.postRepo.create({
+    const post = this.postRepository.create({
       title: createPostDto.title,
       content: createPostDto.content,
-      course: existingClass,
+      course: existingCourse,
       user: foundUser,
     });
-    await this.postRepo.save(post);
+    await this.postRepository.save(post);
 
     return post;
   }
 
-  async readAll(classId: string, userId: string) {
-    const existingClass = await this.classRepo.findOne({
-      where: { id: classId },
+  async readAll(courseId: string, userId: string) {
+    const existingCourse = await this.courseRepository.findOne({
+      where: { id: courseId },
       relations: ['users'],
     });
-    if (!existingClass) throw HttpError.notFound('Clase no encontrada');
+    if (!existingCourse) throw HttpError.notFound('Clase no encontrada');
 
-    const foundUser = await this.userRepo.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['courses'],
     });
     if (!foundUser) throw HttpError.notFound('Usuario no encontrado');
 
-    const isInClass = foundUser.courses.some((c) => c.id === classId);
-    if (!isInClass)
+    const isInCourse = foundUser.courses.some((c) => c.id === courseId);
+    if (!isInCourse)
       throw HttpError.forbidden('El usuario no pertenece a la clase');
 
-    const posts = await this.postRepo.find({
-      where: { course: { id: classId } },
-      relations: ['user', 'user.people', 'class'],
+    const posts = await this.postRepository.find({
+      where: { course: { id: courseId } },
+      relations: ['user', 'user.people', 'Course'],
     });
 
     return posts;
   }
 
-  async readOne(userId: string, classId: string, postId: string) {
-    const existingClass = await this.classRepo.findOne({
-      where: { id: classId },
+  async readOne(userId: string, courseId: string, postId: string) {
+    const existingCourse = await this.courseRepository.findOne({
+      where: { id: courseId },
       relations: ['users'],
     });
-    if (!existingClass) throw HttpError.notFound('Clase no encontrada');
+    if (!existingCourse) throw HttpError.notFound('Clase no encontrada');
 
-    const foundUser = await this.userRepo.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['courses'],
     });
     if (!foundUser) throw HttpError.notFound('Usuario no encontrado');
 
-    const isInClass = foundUser.courses.some((c) => c.id === classId);
-    if (!isInClass)
+    const isInCourse = foundUser.courses.some((c) => c.id === courseId);
+    if (!isInCourse)
       throw HttpError.forbidden('El usuario no pertenece a la clase');
 
-    const post = await this.postRepo.findOne({
-      where: { id: postId, course: { id: classId } },
-      relations: ['user', 'user.people', 'class'],
+    const post = await this.postRepository.findOne({
+      where: { id: postId, course: { id: courseId } },
+      relations: ['user', 'user.people', 'course'],
     });
 
     if (!post) throw HttpError.notFound('Publicación no encontrada');
@@ -94,61 +94,61 @@ export class PostService {
   async update(
     postId: string,
     updatePostDto: UpdatePostDto,
-    classId: string,
+    courseId: string,
     userId: string
   ) {
-    const existingClass = await this.classRepo.findOne({
-      where: { id: classId },
+    const existingCourse = await this.courseRepository.findOne({
+      where: { id: courseId },
       relations: ['users'],
     });
-    if (!existingClass) throw HttpError.notFound('Clase no encontrada');
+    if (!existingCourse) throw HttpError.notFound('Clase no encontrada');
 
-    const foundUser = await this.userRepo.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['courses'],
     });
     if (!foundUser) throw HttpError.notFound('Usuario no encontrado');
 
-    const isInClass = foundUser.courses.some((c) => c.id === classId);
-    if (!isInClass)
+    const isInCourse = foundUser.courses.some((c) => c.id === courseId);
+    if (!isInCourse)
       throw HttpError.forbidden('El usuario no pertenece a la clase');
 
-    const post = await this.postRepo.findOne({
-      where: { id: postId, course: { id: classId }, user: { id: userId } },
+    const post = await this.postRepository.findOne({
+      where: { id: postId, course: { id: courseId }, user: { id: userId } },
     });
     if (!post) throw HttpError.notFound('Publicación no encontrada');
 
     if (updatePostDto.title) post.title = updatePostDto.title;
     if (updatePostDto.content) post.content = updatePostDto.content;
 
-    await this.postRepo.save(post);
+    await this.postRepository.save(post);
 
     return post;
   }
 
-  async delete(postId: string, classId: string, userId: string) {
-    const existingClass = await this.classRepo.findOne({
-      where: { id: classId },
+  async delete(postId: string, courseId: string, userId: string) {
+    const existingCourse = await this.courseRepository.findOne({
+      where: { id: courseId },
       relations: ['users'],
     });
-    if (!existingClass) throw HttpError.notFound('Clase no encontrada');
+    if (!existingCourse) throw HttpError.notFound('Clase no encontrada');
 
-    const foundUser = await this.userRepo.findOne({
+    const foundUser = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['courses'],
     });
     if (!foundUser) throw HttpError.notFound('Usuario no encontrado');
 
-    const isInClass = foundUser.courses.some((c) => c.id === classId);
-    if (!isInClass)
+    const isInCourse = foundUser.courses.some((c) => c.id === courseId);
+    if (!isInCourse)
       throw HttpError.forbidden('El usuario no pertenece a la clase');
 
-    const post = await this.postRepo.findOne({
-      where: { id: postId, course: { id: classId } },
+    const post = await this.postRepository.findOne({
+      where: { id: postId, course: { id: courseId } },
     });
     if (!post) throw HttpError.notFound('Publicación no encontrada');
 
-    await this.postRepo.remove(post);
+    await this.postRepository.remove(post);
 
     return { message: 'Publicación eliminada correctamente' };
   }
